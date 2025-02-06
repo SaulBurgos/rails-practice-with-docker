ca99cd527631bfdec2553990b3b6b3fc0c6b15b2
Saul Burgos Davila <saul@protiv.com>
2025-02-06 15:40:50 -0600

    done with models

--- hello_app/app/models/user.rb
+++ hello_app/app/models/user.rb
@@ -1,3 +1,11 @@
+# Rails uses a concept called "schema reflection". When you generate a model using scaffolding or create a migration, Rails creates a migration file that defines the database schema for your model.
+# The properties (like name and email in this case) are defined in the migration file, which is typically located in db/migrate/YYYYMMDDHHMMSS_create_users.rb.
+# Rails automatically reads the schema from the database and makes these attributes available to your model without you having to explicitly define them in the model file.
+# You can verify this by checking the db/schema.rb file, which reflects the current state of your database schema
+
 class User < ApplicationRecord
     has_many :microposts
+    validates :name,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX } , uniqueness: { case_sensitive: false }
 end
